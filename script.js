6fc80dbe84aab0f578615fd549280ba772e47f90
Michael <mball.28@outlook.com>
2019-08-19 22:41:45 -0600

    display title & comic #

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const comicImg = document.getElementById("comic-image");
 const input = document.getElementById("comic-selector");
 const submitBtn = document.getElementById("submit-btn");
+const comicTitle = document.getElementById("comic-title");
 
 function apiReturnHandler() {
   if (this.readyState == 4 && this.status == 200) {
-    let comicData = JSON.parse(this.responseText);
-    console.log(comicData);
-    comicImg.alt = comicData.alt;
-    comicImg.src = comicData.img;
+    let data = JSON.parse(this.responseText);
+    console.log(data);
+    displayComic(data);
   }
 }
 
@@ -28,13 +28,22 @@ function getComic() {
   xhr.send();
 }
 
+function displayComic(data) {
+  let date = `${data.month}/${data.day}/${data.year}`
+  let currDate = new Date();
+  console.log(currDate);
+  comicImg.alt = data.alt;
+  comicImg.src = data.img;
+  comicTitle.innerText = `#${data.num}: ${data.title}`
+}
+
 getComic();
 
 submitBtn.addEventListener("click", () => {
   getComic();
 });
-input.addEventListener("keydown", (e) => {
-  if(e.keyCode === 13) {
+input.addEventListener("keydown", e => {
+  if (e.keyCode === 13) {
     getComic();
   }
-})
+});
