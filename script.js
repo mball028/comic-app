52ed012e46723363c9f0bf0544dcad456501d52b
Michael <mball.28@outlook.com>
2019-08-19 22:15:34 -0600

    pull in img src from JSON

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+const comicImg = document.getElementById("comic-image");
+const input = document.getElementById("comic-selector");
+const submitBtn = document.getElementById("submit-btn");
+
 function apiReturnHandler() {
   if (this.readyState == 4 && this.status == 200) {
-      let comicData = JSON.parse(this.responseText);
-      console.log(comicData);
+    let comicData = JSON.parse(this.responseText);
+    console.log(comicData);
+    comicImg.alt = comicData.alt;
+    comicImg.src = comicData.img;
   }
 }
 
 // Call the comic API
 
 function getComic() {
-  let url = "https://xkcd.com/info.0.json";
+  const proxy = "https://cors-anywhere.herokuapp.com/";
+  let apiURL = "https://xkcd.com/info.0.json";
+
+  if (input.value) {
+    apiURL = `https://xkcd.com/${input.value}/info.0.json`;
+  }
+  let url = proxy + apiURL;
 
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = apiReturnHandler;
   xhr.open("GET", url, true);
-  xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
   xhr.send();
 }
 
-
 getComic();
+
+submitBtn.addEventListener("click", () => {
+  getComic();
+});
+input.addEventListener("keydown", (e) => {
+  if(e.keyCode === 13) {
+    getComic();
+  }
+})
